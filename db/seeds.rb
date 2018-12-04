39566277e53b28621779c37aac46f4ee79287719
Juliette Chevalier <juliettech.1393@gmail.com>
2018-12-04 11:39:24 -0500

    seeds created. only missing being able to destroy Skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "Destroying SkillsUsers..."
+SkillsUser.destroy_all
+p "Destroying Users..."
+User.destroy_all
+# p "Destroying Skills..."
+# Skill.destroy_all
+
+p "Creating Skills...."
+football = Skill.create!(name: "Football")
+basketball = Skill.create!(name: "Basketball")
+foot = Skill.create!(name: "Foot", parent: football)
+soccer = Skill.create!(name: "Soccer", parent: football)
+basket = Skill.create!(name: "Basket", parent: basketball)
+
+p "Creating Users...."
+jules = User.create!(email: "jules@agorize.org", password: "secret", points: 100)
+pierre = User.create!(email: "pierre@agorize.org", password: "secret", points: 200)
+julian = User.create!(email: "julian@agorize.org", password: "secret", points: 100)
+aida = User.create!(email: "aida@agorize.org", password: "secret", points: 50)
+monique = User.create!(email: "monique@agorize.org", password: "secret", points: 10)
+
+p "Creating SkillsUsers...."
+SkillsUser.create!(skill: football, user: jules)
+SkillsUser.create!(skill: football, user: pierre)
+SkillsUser.create!(skill: foot, user: julian)
+SkillsUser.create!(skill: basketball, user: aida)
+SkillsUser.create!(skill: soccer, user: monique)
+
